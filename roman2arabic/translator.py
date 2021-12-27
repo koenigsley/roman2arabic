ca61790f7422d5c81d1d02a85e4830c216b417d2
Mikhail Eremeev <meremeev@sfedu.ru>
2021-12-27 13:12:08 +0300

    feat: add translator

--- roman2arabic/translator.py
+++ roman2arabic/translator.py
@@ -1,5 +1,61 @@
+import re
+import textwrap
+
 
 class Translator:
+    # https://ru.wikipedia.org/wiki/Римские_цифры#Регулярные_выражения
+    ROMAN_REGEX = re.compile(
+        '^(M{0,3})(D?C{0,3}|C[DM])(L?X{0,3}|X[LC])(V?I{0,3}|I[VX])$'
+    )
+
+    # https://ru.wikipedia.org/wiki/Римские_цифры#Римские_цифры
+    __ADDITION_RULES = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
+    __SUBTRACTION_RULES = {
+        'IV': 2,
+        'IX': 2,
+        'XL': 20,
+        'XC': 20,
+        'CD': 200,
+        'CM': 200,
+    }
+
+    class TranslatorError(Exception):
+        pass
+
+    class NumeralsAreNotRomansError(TranslatorError):
+        pass
+
     @staticmethod
     def translate_to_arabic(roman_numerals: str) -> int:
-        pass
+        Translator.__ensure_is_roman(roman_numerals)
+
+        result = 0
+
+        for chunk in Translator.__chunked(roman_numerals):
+            for symbol in chunk:
+                result += Translator.__ADDITION_RULES[symbol]
+            result -= Translator.__SUBTRACTION_RULES.get(chunk, 0)
+
+        return result
+
+    @staticmethod
+    def is_roman(numerals: str) -> bool:
+        return bool(Translator.ROMAN_REGEX.match(numerals))
+
+    @staticmethod
+    def __ensure_is_roman(numerals: str) -> None:
+        if not Translator.is_roman(numerals):
+            message = f'Numerals should match pattern {Translator.ROMAN_REGEX.pattern}'
+            raise Translator.NumeralsAreNotRomansError(message)
+
+    @staticmethod
+    def __chunked(string: str) -> 'list[str]':
+        return textwrap.wrap(string, 2)
