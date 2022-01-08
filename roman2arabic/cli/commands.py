11bc5ba5e6836ab6fc8d8e77c2f091ea3f7f01ba
Mikhail Eremeev <meremeev@sfedu.ru>
2022-01-09 00:12:57 +0300

    refactor: rename arabic_numbers -> arabic_number

--- roman2arabic/cli/commands.py
+++ roman2arabic/cli/commands.py
@@ -4,5 +4,5 @@ from .handlers import catch_translator_error
 
 @catch_translator_error
 def translate(roman_numerals: str) -> None:
-    arabic_numbers = Translator.translate_to_arabic(roman_numerals)
-    print(arabic_numbers)
+    arabic_number = Translator.translate_to_arabic(roman_numerals)
+    print(arabic_number)
